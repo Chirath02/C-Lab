7b57f66d096ca68f33a0c138233c669f8a9cd1e3
Chirath <chirath.02@gmail.com>
2015-08-14 15:08:09 -0700

    Did a very big program 2.cpp and 3b2.cpp

--- lab4/2.cpp
+++ lab4/2.cpp
@@ -1,41 +1,68 @@
-#include <iostream>
+#include<iostream>
+#include<cstring>
+#include<stdlib.h>
 using namespace std;
-class phone
-{
-public:
-    int area ;
-    int exc;
-    long num;
-    
-    void intializ (int x ,int y , long z)
-    {
-        area=x;
-        exc=y;
-        num=z;
-        cout<<"MY NUMBER IS  =  "<<area<<exc<<num<<endl;
-    }
-    void input()
-    {
-        cout<<"ENTER YOUR NUMBER"<<endl;
-        cout<<"ENTER three parts of a phone number separately (area ,exc,num)"<<endl;
-        cin>>area;
-        cin>>exc;
-        cin>>num;
-    }
-    void disp()
-    {
-        cout<<"YOUR NUMBER IS    =   "<<area<<exc<<num<<endl;
-    }
-};
-int main()
-{
-    phone p1 ,p2 ;
-    p2.input();
-    p2.disp();
-    p1. intializ (91, 124, 2304871);
-    
-    return 0;
+time_t t = time(0);   // get time now
+struct tm * now = localtime( & t );
+long int convert_to_days(int day,int month,int year) {
+    return (year*365)+(month*30)+(day);
+}
+void age(long int a,long int b,string name) {
+    long int c = b - a + convert_to_days(0,1,1900);
+    int year,month,day;
+    year = c/(365);
+    month = (c%(365))/(30);
+    day = (c%(30));
+    cout<<"Hey "<<name<<" !Your age is ";
+    cout<<year<<" years,"<<month<<" months,"<<day<<" days"<<endl;
+    cout<<"Your Age in Days:"<<c<<" days since your birth"<<endl;
+    cout<<"Your Age in Hours:"<<c*24<<" hours since your birth"<<endl;
+    cout<<"Your Age in Minutes:"<<c*24*60<<" minutes since your birth"<<endl;
+    cout<<365-(c/365)<<" days left for your next birthday"<<endl<<endl;
 }
 
-
-
+struct dob {
+    int day,month,year;
+    long int age_in_days;
+    char name[100];
+    void display() {
+        age(age_in_days,convert_to_days(now->tm_mday,now->tm_mon,now->tm_year),name);
+    }
+};
+int main() {
+    dob d[100],temp;
+    char ch;
+    int day,month,year,n,i=0;
+    do {
+        cout<<"1 to enter a person"<<endl;
+        cout<<"2 to sort according to the descending order of age"<<endl;
+        cout<<"3 to display all"<<endl;
+        cout<<"0 to exit"<<endl;
+        cin>>n;
+        if(n==1) {
+            cout<<"Enter name:";
+            cin>>d[i].name;
+            cout<<"Enter the date of birth (dd/mm/yyyy):";
+            cin>>d[i].day>>d[i].month>>d[i].year;
+            d[i].age_in_days=convert_to_days(d[i].day,d[i].month,d[i].year);
+            i++;
+        }
+        else if(n==2) {
+            for(int j=0;j<i;++j) {
+                if(d[j].age_in_days<d[j+1].age_in_days) {
+                    temp = d[j];
+                    d[j] = d[j+1];
+                    d[j+1] = temp;
+                }
+            }
+            system("clear");
+            cout<<"Sorting done..."<<endl;
+        }
+        else if(n==3) {
+            system("clear");
+            for(int j=0;j<i;++j)
+                d[j].display();
+        }
+    }while(n!=0);
+    return 0;
+}
--- lab4/3b2.cpp
+++ lab4/3b2.cpp
@@ -1,21 +1,16 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-string show(int x) {
-    if(x) 
-        return "True ";
-    return "False";
-}
 int main() {
     int a=1,b=1;
-    cout<<"  A  |"<<"  B  |"<<" A and B |"<<" A or B |"<<endl;
+    cout<<"  x   "<<"  y  |"<<" x XOR y "<<endl;
     for(int i=0;i<4;++i) {
-        if(i==1 || i==3) 
+        if(i==1 || i==3)
             b--;
         if(i==2) {
             a-- ; b++;
         }
-        cout<<show(a)<<"|"<<show(b)<<"|  "<<show(a&b)<<"  |  "<<show(a|b)<<" |"<<endl;
+        cout<<"  "<<a<<"     "<<b<<"  |    "<<(a^b)<<endl;
     }
     return 0;
 }
